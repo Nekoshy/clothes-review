c75507b2feaf633d7f8c4d88c4dad6912bf961f2
Nekoshy <neko@intmail.pl>
2022-04-01 18:49:59 +0200

    shops page in progress

---
+++ pages/addShop.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/navbar'
+
+const AddShop = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                OK
+            </div>
+        </div>
+    )
+}
+
+export default AddShop
--- pages/shops.js
+++ pages/shops.js
@@ -4,10 +4,25 @@ const Shops = () => {
     return(
         <div>
             <Navbar/>
-            <h1>
-                Sklepy
-            </h1>
-            <p>:)</p>
+            <div className="container">
+                <div className="row">
+                    <h1>
+                        Lista sklepów
+                    </h1>
+                </div>
+                <div className="row">
+                    <div className="d-flex">
+                        <div className="orangeBorder">
+                            <p style={{margin:'0 0 0 10px'}}>
+                                Brakuje jakiegoś sklepu? Pomóż nam uzupełnić bazę!
+                            </p>
+                            <a href='/addShop'>
+                                <button className="orangeButton br-20 ml-10" style={{width:'120px', height:'40px', margin:'10px 10px 10px 10px'}}>Dodaj!</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
